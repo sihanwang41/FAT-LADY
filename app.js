27591e5c8c742ca3d4e5eea087552578f9edf682
JIALUN LIU <jl4347@columbia.edu>
2015-03-09 21:57:26 -0400

    Add initial test

--- app.js
+++ app.js
@@ -5,6 +5,4 @@ app.get('/', function(request, response){
 	response.send("OK");
 });
 
-app.listen(3000, function(){
-	console.log("Listening on Port 3000");
-});
+module.exports = app;
---
+++ test.js
@@ -0,0 +1,14 @@
+var request = require('supertest');
+var app = require('./app');
+
+describe('Request to the root path', function(){
+	it('Returns a 200 status code', function(done){
+		request(app)
+			.get('/')
+			.expect(200)
+			.end(function(error){
+				if (error) throw error;
+				done();
+			});//Function chaining
+	});
+});
