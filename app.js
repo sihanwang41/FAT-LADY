290305e2e0fb61a9e590788ac478ff5e758fcd79
JIALUN LIU <jl4347@columbia.edu>
2015-03-09 23:49:57 -0400

    Import Redis into the app.js

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 var express = require('express');
 var app = express();
 
+var redis = require('redis');
+var client = redis.createClient();
+
+// Now store the API key and API call into the Redis
+
 // GET on '/'
 app.get('/', function(request, response){
 	response.send("OK");
