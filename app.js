fd9a62669b67a9c342c99e7b13c6fd8ec885d103
JIALUN LIU <jl4347@columbia.edu>
2015-03-09 22:55:56 -0400

    Added the /customers

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+// Loading the express library
 var express = require('express');
 var app = express();
 
+// GET on '/'
 app.get('/', function(request, response){
 	response.send("OK");
 });
 
+// GET on '/customers'
+app.get('/customers', function(request, response){
+	response.json("OK");
+});
+
+// export the module so that it could be called elsewhere
 module.exports = app;
--- test.js
+++ test.js
@@ -5,10 +5,26 @@ describe('Request to the root path', function(){
 	it('Returns a 200 status code', function(done){
 		request(app)
 			.get('/')
-			.expect(200)
-			.end(function(error){
-				if (error) throw error;
-				done();
-			});//Function chaining
+			.expect(200, done);
 	});
+});
+
+describe('Listing customers on /customers', function() {
+	it('Returns 200 status code', function(done){
+		request(app)
+			.get('/customers')
+			.expect(200, done);
+	});
+
+	it('Returns JSON format', function(done){
+		request(app)
+			.get('/customers')
+			.expect('Content-Type', /json/, done); // json is the regex
+	});
+
+	// it('Returns initial customers', function(done){
+	// 	request(app)
+	// 		.get('/customers')
+	// 		.expect();
+	// });
 });
