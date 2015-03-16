c751efe5c9883ed381bb7ecf8b0d0c36f1997936
JIALUN LIU <jl4347@columbia.edu>
2015-03-16 15:09:20 -0400

    Function to sort the order of middleware to be executed

--- app.js
+++ app.js
@@ -8,25 +8,25 @@ var service = require('./routes/index');
 // Testing configurable middleware
 var confirguration = {
 	before1: {
-		prioty: 100,
+		priority: 100,
 		enable: true
 	},
 	before2: {
-		prioty: 80,
+		priority: 80,
 		enable: true
 	},
-	before1: {
-		prioty: 50,
+	service: {
+		priority: 50,
 		enable: true
 	},
-	before1: {
-		prioty: 30,
+	after1: {
+		priority: 30,
 		enable: true
 	},
-	before1: {
-		prioty: 10,
+	after2: {
+		priority: 10,
 		enable: true
-	},
+	}
 }
 
 var before1 = require('./example_middleware/before1');
@@ -34,9 +34,28 @@ var before2 = require('./example_middleware/before2');
 var after1 = require('./example_middleware/after1');
 var after2 = require('./example_middleware/after2');
 
+// Function to sort the order of the middleware to be executed
+var sortedConfig = [];
+
+var sortConfig = function(confirguration){
+	var sortable = [];
+	for (var middleware in confirguration)
+	    sortable.push([middleware, confirguration[middleware]['priority']]);
+
+	sortable.sort(function(a, b) {return b[1] - a[1]});
+	return sortable;
+}
+
+// Fake request to simulate the /service
 var fakeRequest = function(request, response, next){
 	response.send(200);
 	next();
+}
+
+sortedConfig = sortConfig(confirguration);
+
+for (var middlewareName in confirguration){
+
 }
 
 
