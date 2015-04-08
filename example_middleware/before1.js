a1c68648c17dc690051742f00ec984329f065c46
JIALUN LIU <jl4347@columbia.edu>
2015-04-08 16:06:12 -0400

    Testing error handler in before1.js

--- example_middleware/before1.js
+++ example_middleware/before1.js
@@ -5,7 +5,15 @@ var router = express.Router();
 
 router.use(function(request, response, next){
 		console.log('This is middleware BEFORE1');
-		next();
+		var success = false;
+		if (!success){
+			// err.sourceofError = 'BEFORE1';
+			return next(new Error('Something blew up!!!'));
+		}
+		else{
+			next();
+		}
+		// next();
 	});
 
 
