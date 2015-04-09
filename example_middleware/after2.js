6cdc1baa66a4b2b4eeb7e1d17ad52d002a65a89a
sareenarazak <sareenarazak@users.noreply.github.com>
2015-04-09 16:26:42 -0400

    Create after2.js

---
+++ example_middleware/after2.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+router.use(function(request, response, next){
+		console.log('This is middleware AFTER2');
+		// console.log(response.content);
+		// response.sendStatus(200);
+		next();
+	});
+
+
+module.exports = router;
