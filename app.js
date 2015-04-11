ff2c49176e3d560569c00feef68a5461b8087d60
sareenarazak <Sareena@dyn-129-236-214-109.dyn.columbia.edu>
2015-04-11 14:28:46 -0400

    Revert "Nesting async operations failed"
    This reverts commit 3b1412d731f12d91d8b35f4a078056796c5feb50.
    
    Revert my changes to wrong branch

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ var sortConfig = function(confirguration){
 function configurableMiddleWare(req, res, next) {
    console.log('here');
    	var operations = [];
-   	var operationsLog = [];
 
    	var middleware;
    	var config = req.configuration;
@@ -97,17 +96,7 @@ function configurableMiddleWare(req, res, next) {
    // now actually invoke the middleware in series
    	async.series(operations, function(err) {
    		if(err) {
-   	    	// operationsLog.push(logError.bind(null, err, req, res));
-   	    	// operationsLog.push(errorHandler.bind(null, err, req, res));
-
-   	    	// console.log("Something blew up!!!!!!!")
-   	    	// async.series(operationsLog, function(err){
-   	    	// 	if (err){
-   	    	// 		return next(err);
-   	    	// 	}
-
-   	    	// 	next();
-   	    	// });
+   	    	console.log('Something blew up!!!!!!');
    			return next(err);
    	  	}
    	  	console.log('middleware get executed');
