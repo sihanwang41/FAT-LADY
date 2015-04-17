ddf91b0b12aed0ab3f9309699260352d97489d40
JIALUN LIU <jl4347@columbia.edu>
2015-04-17 02:13:21 -0400

    Reindent configuration.js and auth.js

--- example_middleware/auth.js
+++ example_middleware/auth.js
@@ -10,93 +10,93 @@ AWS.config.logger = console;
 var db = new AWS.DynamoDB();
 
 router.use(function(req, res, next) {
-  var auth;
-  var username;
-  var password;
+    var auth;
+    var username;
+    var password;
 
-  if (res.statuscode == 304 || res.statuscode == 412 || res.statuscode == 403 || res.statuscode == 404 || res.statuscode == 401)
-  {
-    next();
-  }
-  // check whether an autorization header was send    
-  if (req.headers.authorization) {
+    if (res.statuscode == 304 || res.statuscode == 412 || res.statuscode == 403 || res.statuscode == 404 || res.statuscode == 401)
+    {
+        next();
+    }
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
     // only accepting basic auth, so:
     // * cut the starting "Basic " from the header
     // * decode the base64 encoded username:password
     // * split the string at the colon
     // -> should result in an array
     auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
-  }
-
-
-  if (auth ) {
-
-      // check if first value matches the expected user on dynamodb aws
-      // and if second value matches the expected password on dynamodb aws
-      username = auth[0];
-      password = auth[1];
-
-  var params = {
-    KeyConditions: { 
-      user: {
-        ComparisonOperator: 'EQ', 
-        AttributeValueList: [
-          { 
-            S: username
-          }
-        ]
-      }
-    },
-    TableName: 'users', 
-    AttributesToGet: [
-      'user','password'
-    ],
-    QueryFilter: {
-      password: {
-        ComparisonOperator: 'EQ', 
-        AttributeValueList: [
-          { 
-            S: password
-          }
-        ]
-      },
     }
-  };
 
-  // Authenticator
-  db.query(params, function(err, data) {
-    if (err)
+
+    if (auth ) {
+
+        // check if first value matches the expected user on dynamodb aws
+        // and if second value matches the expected password on dynamodb aws
+        username = auth[0];
+        password = auth[1];
+
+        var params = {
+            KeyConditions: { 
+                user: {
+                    ComparisonOperator: 'EQ', 
+                    AttributeValueList: [
+                    { 
+                        S: username
+                    }
+                    ]
+                }
+            },
+            TableName: 'users', 
+            AttributesToGet: [
+            'user','password'
+            ],
+            QueryFilter: {
+                password: {
+                    ComparisonOperator: 'EQ', 
+                    AttributeValueList: [
+                    { 
+                        S: password
+                    }
+                    ]
+                },
+            }
+        };
+
+        // Authenticator
+        db.query(params, function(err, data) {
+            if (err)
+            {
+            //console.log(err, err.stack); // an error occurred
+            console.log("error on authentication")
+            console.log("not authorized")
+            }	
+            else     
+            {
+                if(data.Count) // returned array > 0
+                {
+                    console.log("authenticated")
+                    console.log("authorized")
+                    next();
+                }
+                else
+                {
+                    res.statuscode = 401;
+                    console.log("Wrong Login/password, not authenticated")
+                    console.log("not authorized")
+                }
+            }
+        });
+
+    }
+    else
     {
-      //console.log(err, err.stack); // an error occurred
-       console.log("error on authentication")
-       console.log("not authorized")
-    }	
-    else     
-    {
-    		if(data.Count) // returned array > 0
-    		{
-    			console.log("authenticated")
-    			console.log("authorized")
-     	    next();
-    		}
-    		else
-    		{
-    			res.statuscode = 401;
-          console.log("Wrong Login/password, not authenticated")
-          console.log("not authorized")
-       	}
-  	}
-  });
+        //next(req);
 
-  }
-  else
-  {
-    //next(req);
+        console.log("authentication header empty")
+        console.log("not authorized")
+    }
 
-    console.log("authentication header empty")
-    console.log("not authorized")
-  }
-      
-  });
+});
 
 module.exports = router;
--- example_middleware/configuration.js
+++ example_middleware/configuration.js
@@ -1,80 +1,78 @@
 'use strict';
 var express = require('express');
 var router = express.Router();
-console.log("Getting middlewareConfig")
+var AWS = require('aws-sdk');
 
 router.use(function (req,res,next){
-var AWS = require('aws-sdk');
-AWS.config.loadFromPath('./configAuthConfig.json');
-AWS.config.update({region: 'us-west-2'});
-AWS.config.logger = console;
 
-var db = new AWS.DynamoDB();
-var action = req.method.toLowerCase();
-if(action){
+    AWS.config.loadFromPath('./configAuthConfig.json');
+    AWS.config.update({region: 'us-west-2'});
+    AWS.config.logger = console;
 
-  //Params for db query
-  var params = {
-      KeyConditions: { /* required */
-      action: {
-        ComparisonOperator: 'EQ', /* required */
-        AttributeValueList: [
-          { 
-              S:action
-          }
-      ]
-    }
-  },
-  TableName: 'middlewareConfig', /* required */
-};
-}
+    var db = new AWS.DynamoDB();
+    var action = req.method.toLowerCase();
+    if(action){
 
-// get the configuration
-db.query(params, function (err, data) {
-  if(err) console.log(err, err.stack); // an error occurred
-  else     
-  {
-     if(data.Count) // returned array > 0
-      {
-           var config = {
-            auth:{priority:0,enable:false},
-            Nonce:{priority:0,enable:false},
-            loggingBefore:{priority:0,enable:false},
-            etagBefore:{priority:0,enable:false},
-            service:{priority:0,enable:false},
-            loggingAfter:{priority:0,enable:false},
-            etagAfter:{priority:0,enable:false}
+        //Params for db query
+        var params = {
+            KeyConditions: { /* required */
+                action: {
+                    ComparisonOperator: 'EQ', /* required */
+                    AttributeValueList: [
+                    { 
+                    S:action
+                    }
+                    ]
+                }
+            },
+            TableName: 'middlewareConfig', /* required */
+        };
+    }   
 
+    // get the configuration
+    db.query(params, function (err, data) {
+        if(err) console.log(err, err.stack); // an error occurred
+        else     
+        {
+            if(data.Count) // returned array > 0
+            {
+                var config = {
+                    auth:{priority:0,enable:false},
+                    Nonce:{priority:0,enable:false},
+                    loggingBefore:{priority:0,enable:false},
+                    etagBefore:{priority:0,enable:false},
+                    service:{priority:0,enable:false},
+                    loggingAfter:{priority:0,enable:false},
+                    etagAfter:{priority:0,enable:false}
+                };
 
-         };
-
-         config.auth.priority = data.Items[0].auth.L[0].N;
-         config.auth.enable = data.Items[0].auth.L[1].BOOL;
-         config.Nonce.priority = data.Items[0].Nonce.L[0].N;
-         config.Nonce.enable = data.Items[0].Nonce.L[1].BOOL;
-         config.loggingBefore.priority = data.Items[0].loggingBefore.L[0].N;
-         config.loggingBefore.enable = data.Items[0].loggingBefore.L[1].BOOL;
-         config.etagBefore.priority = data.Items[0].etagBefore.L[0].N;
-         config.etagBefore.enable = data.Items[0].etagBefore.L[1].BOOL;
-         config.service.priority = data.Items[0].service.L[0].N;
-         config.service.enable = data.Items[0].service.L[1].BOOL;
-         config.loggingAfter.priority = data.Items[0].loggingAfter.L[0].N;
-         config.loggingAfter.enable = data.Items[0].loggingAfter.L[1].BOOL;
-         config.etagAfter.priority = data.Items[0].etagAfter.L[0].N;
-         config.etagAfter.enable = data.Items[0].etagAfter.L[1].BOOL;
+                config.auth.priority = data.Items[0].auth.L[0].N;
+                config.auth.enable = data.Items[0].auth.L[1].BOOL;
+                config.Nonce.priority = data.Items[0].Nonce.L[0].N;
+                config.Nonce.enable = data.Items[0].Nonce.L[1].BOOL;
+                config.loggingBefore.priority = data.Items[0].loggingBefore.L[0].N;
+                config.loggingBefore.enable = data.Items[0].loggingBefore.L[1].BOOL;
+                config.etagBefore.priority = data.Items[0].etagBefore.L[0].N;
+                config.etagBefore.enable = data.Items[0].etagBefore.L[1].BOOL;
+                config.service.priority = data.Items[0].service.L[0].N;
+                config.service.enable = data.Items[0].service.L[1].BOOL;
+                config.loggingAfter.priority = data.Items[0].loggingAfter.L[0].N;
+                config.loggingAfter.enable = data.Items[0].loggingAfter.L[1].BOOL;
+                config.etagAfter.priority = data.Items[0].etagAfter.L[0].N;
+                config.etagAfter.enable = data.Items[0].etagAfter.L[1].BOOL;
 
 
-           req.configuration = config;
-           console.log(config);
-           next();
+                req.configuration = config;
+                console.log(config);
+                next();
 
-      }
-    
-    else
-    {
-      console.log("Unable to get the middlewareConfig");
-    }
-  }
+            }
+
+            else
+            {
+                console.log("Unable to get the middlewareConfig");
+            }
+        }
     });
 });
 module.exports = router;
